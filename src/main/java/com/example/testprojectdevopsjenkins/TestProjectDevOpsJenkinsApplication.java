cd328278b001dd2ffde7797d2bad1a1472f010dd
Mykhasko Viktor <mikhasko@msn.com>
2021-11-15 11:59:20 +0200

    Initial commit

---
+++ src/main/java/com/example/testprojectdevopsjenkins/TestProjectDevOpsJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprojectdevopsjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectDevOpsJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectDevOpsJenkinsApplication.class, args);
+    }
+
+}
